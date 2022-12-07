3c2df1218a70f1f4d5945aa92903b30e8e76a5e6
epicGG17 <gisellefuentes45@gmail.com>
2022-12-07 01:04:49 -0800

    Facncyifmytext

---
+++ fancitymytext.js
@@ -0,0 +1,8 @@
+document.getElementById('bigButton').onclick = outputMessage;
+
+//bigButton.onclick = outputMessage;
+
+function outputMessage() {
+    console.log("click");
+    alert("Hello World!");
+}
